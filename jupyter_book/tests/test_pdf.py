955ba46b0b31d13543233bf8dfd89d03dc5ff2e1
Matt McKay <mamckay@gmail.com>
2020-04-15 11:43:11 +1000

    fix test for pdf to call pdfhtml

--- jupyter_book/tests/test_pdf.py
+++ jupyter_book/tests/test_pdf.py
@@ -9,7 +9,7 @@ path_tests = Path(__file__).parent
 def test_html_pdf(tmpdir):
     path_output = Path(tmpdir).absolute()
     path_template = path_tests.parent.joinpath("book_template")
-    cmd = f"jb build {path_template} --path-output {path_output} --build pdf_html"
+    cmd = f"jb build {path_template} --path-output {path_output} --build pdfhtml"
     run(cmd.split(), check=True)
     path_html = path_output.joinpath("_build", "html")
     path_pdf = path_output.joinpath("_build", "pdf")
